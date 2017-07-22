63a3178441b63fae29798e16bdd68b7131b9410f
Nick Kinlen <nick_kinlen@hotmail.com>
2017-07-22 01:59:50 -0400

    Added shadows and header, new js

--- js.js
+++ js.js
@@ -1,5 +1,96 @@
 $(document).ready(function(){
-    $(".button").click(function(){
-        $(this).hide();
+
+    $('#parenthesis-left').click(function(){
+        $('#displayText').append('( ');
     });
+
+    $('#parenthesis-right').click(function(){
+        $('#displayText').append(') ');
+    });
+
+    $('#percentage').click(function(){
+        $('#displayText').append('% ');
+    });
+
+    $('#clear').click(function(){
+        $('#displayText').empty();
+    });
+
+    $('#7').click(function(){
+        $('#displayText').append('7 ');
+    });
+
+    $('#8').click(function(){
+        $('#displayText').append('8 ');
+    });
+
+    $('#9').click(function(){
+        $('#displayText').append('9 ');
+    });
+
+    $('#divide').click(function(){
+        $('#displayText').append('/ ');
+    });
+
+    $('#4').click(function(){
+        $('#displayText').append('4 ');
+    });
+
+    $('#5').click(function(){
+        $('#displayText').append('5 ');
+    });
+
+    $('#6').click(function(){
+        $('#displayText').append('6 ');
+    });
+
+    $('#multiply').click(function(){
+        $('#displayText').append('* ');
+    });
+
+    $('#1').click(function(){
+        $('#displayText').append('1 ');
+    });
+
+    $('#2').click(function(){
+        $('#displayText').append('2 ');
+    });
+
+    $('#3').click(function(){
+        $('#displayText').append('3 ');
+    });
+
+    $('#subtract').click(function(){
+        $('#displayText').append('- ');
+    });
+
+    $('#0').click(function(){
+        $('#displayText').append('0 ');
+    });
+
+    $('#decimal').click(function(){
+        $('#displayText').append('. ');
+    });
+
+    $('#equals').click(function(){
+        $('#displayText').append('= ');
+    });
+
+    $('#plus').click(function(){
+        $('#displayText').append('+ ');
+    });
+
+
+    var keys = document.querySelectorAll('.button span');
+    var operators = ['+', '-', '*', '/'];
+    var decimalAdded = false;
+
+    for(var i = 0; i < keys.length; i++){
+      keys[i].onclick = function(e){
+        var input = document.querySelector('#displayText');
+        var inputVal = input.innerHTML;
+      }
+      console.log(inputVal);
+    }
+
 });
