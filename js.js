47109e6077f109e9012f2eae9282f947a79321f6
Nick Kinlen <nick_kinlen@hotmail.com>
2017-07-29 16:09:56 -0400

    Basic functionality of calculator done

--- js.js
+++ js.js
@@ -1,96 +1,27 @@
-$(document).ready(function(){
+window.onload=function(){
+  var elem = document.querySelectorAll(".button2");
+  var display = document.getElementById("displayText");
+  var result = "";
 
-    $('#parenthesis-left').click(function(){
-        $('#displayText').append('( ');
-    });
+  for(var i = 0; i < elem.length; i++){
+    elem[i].addEventListener("click",function(){
+      if(this.innerHTML=='='){
+        result = eval(display.innerHTML);
 
-    $('#parenthesis-right').click(function(){
-        $('#displayText').append(') ');
-    });
+        display.innerHTML = result;
+        
+      }else if(this.innerHTML == 'CE'){
+        result="";
 
-    $('#percentage').click(function(){
-        $('#displayText').append('% ');
-    });
+        display.innerHTML = result;
+      }else{
+        result += this.innerHTML;
 
-    $('#clear').click(function(){
-        $('#displayText').empty();
-    });
+        display.innerHTML = result;
 
-    $('#7').click(function(){
-        $('#displayText').append('7 ');
-    });
-
-    $('#8').click(function(){
-        $('#displayText').append('8 ');
-    });
-
-    $('#9').click(function(){
-        $('#displayText').append('9 ');
-    });
-
-    $('#divide').click(function(){
-        $('#displayText').append('/ ');
-    });
-
-    $('#4').click(function(){
-        $('#displayText').append('4 ');
-    });
-
-    $('#5').click(function(){
-        $('#displayText').append('5 ');
-    });
-
-    $('#6').click(function(){
-        $('#displayText').append('6 ');
-    });
-
-    $('#multiply').click(function(){
-        $('#displayText').append('* ');
-    });
-
-    $('#1').click(function(){
-        $('#displayText').append('1 ');
-    });
-
-    $('#2').click(function(){
-        $('#displayText').append('2 ');
-    });
-
-    $('#3').click(function(){
-        $('#displayText').append('3 ');
-    });
-
-    $('#subtract').click(function(){
-        $('#displayText').append('- ');
-    });
-
-    $('#0').click(function(){
-        $('#displayText').append('0 ');
-    });
-
-    $('#decimal').click(function(){
-        $('#displayText').append('. ');
-    });
-
-    $('#equals').click(function(){
-        $('#displayText').append('= ');
-    });
-
-    $('#plus').click(function(){
-        $('#displayText').append('+ ');
-    });
-
-
-    var keys = document.querySelectorAll('.button span');
-    var operators = ['+', '-', '*', '/'];
-    var decimalAdded = false;
-
-    for(var i = 0; i < keys.length; i++){
-      keys[i].onclick = function(e){
-        var input = document.querySelector('#displayText');
-        var inputVal = input.innerHTML;
       }
-      console.log(inputVal);
-    }
+    });
 
-});
+  }
+
+};
